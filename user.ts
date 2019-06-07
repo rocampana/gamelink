afb847688303705193b3f39c0297f064164df1c5
Rafael Campana <rafael.campana@brlink.com.br>
2019-06-07 19:13:05 -0300

    first commit

---
+++ user.ts
@@ -0,0 +1,24 @@
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import 'source-map-support/register';
+
+export const create: APIGatewayProxyHandler = async (event, _context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+      input: event,
+    }, null, 2),
+  };
+}
+
+export const update: APIGatewayProxyHandler = async (event, _context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+      input: event,
+    }, null, 2),
+  };
+}
+
+
